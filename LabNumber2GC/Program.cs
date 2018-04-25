a36ebf1de39b99a2e05d8515b967201d89eb4a5b
Riyad <rayabed@gmail.com>
2018-04-25 11:43:14 -0400

    First Commit

---
+++ LabNumber2GC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LabNumber2GC
+{
+    class Program
+    {
+        public static double Area { get; private set; }
+
+        private static double Perimeter;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+            Console.WriteLine("Please enter length: ");
+            double length = (Double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Please enter width: ");
+            double width = (Double.Parse(Console.ReadLine()));
+
+
+
+            Area = (length * width);
+            Perimeter = 2 * (length + width);
+            Console.WriteLine("The area is: " + Area);
+            Console.WriteLine("The perimeter is: " + Perimeter);
+
+        }
+        
+    }
+}
